f9c2aabd6287bbbd8d026c393b324eb643672020
dinhluc79pq <dinhluc79pq>
2024-11-04 15:57:56 +0700

    Fix code

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     let num_photos_printed = -1;
-    $.ajax({
-        type: "GET",
-        url: "/get_photos_printed",
-        success: function (response) {
-            file_count = response.file_count
-            if(file_count != num_photos_printed) {
-                $('#photos_printed').append(file_count + ' Hình ảnh')
-                var cost = file_count * 150000
-                $('#total_cost').append(new Intl.NumberFormat('de-DE').format(cost) + ' VNĐ')
+    function fetchPhotosPrinted() {
+        $.ajax({
+            type: "GET",
+            url: "/get_photos_printed",
+            success: function (response) {
+                file_count = response.file_count
+                if(file_count != num_photos_printed) {
+                    $('#photos_printed').html(file_count + ' Hình Ảnh')
+                    var cost = file_count * 150000
+                    $('#total_cost').html(new Intl.NumberFormat('de-DE').format(cost) + ' VNĐ')
+                }
+                else {
+                    alert('error')
+                }
             }
-            else {
-                alert('error')
-            }
-        }
-    });
-
+        });
+    }
+    fetchPhotosPrinted();
+    setInterval(fetchPhotosPrinted, 60000);
 });
