c4c98c1ce96ef28227598a9b777b679fabb5652b
dinhluc79pq <dinhluc79pq>
2024-11-05 12:19:44 +0700

    Fix count photos printed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify
+from datetime import datetime
 
 import os
 
@@ -10,13 +11,19 @@ def index():
 
 @app.route('/get_photos_printed', methods=['GET'])
 def get_photos_printed():
-    folder_path_pos1 = r'\\Coasterpos2\prints\Archive\2024.11.04\s6x8'
-    folder_path_pos2 = r'\\Coasterpos2\prints\Archive\2024.11.04\s6x8'
-    folder_path_pos3 = r'\\Coasterpos2\prints\Archive\2024.11.04\s6x8'
+    formatted_date = datetime.now().strftime("%Y.%m.%d")
 
-    photos_printed_pos1 = get_count_files(folder_path_pos1)
-    photos_printed_pos2 = get_count_files(folder_path_pos2)
-    photos_printed_pos3 = get_count_files(folder_path_pos3)
+    folder_path_pos1 = r'\\Coasterpos1\prints\Archive'
+    folder_path_pos2 = r'\\Coasterpos2\prints\Archive'
+    folder_path_pos3 = r'\\Coasterpos3\prints\Archive'
+
+    path_pos1 = folder_path_pos1 + f'\\{formatted_date}\\s6x8'
+    path_pos2 = folder_path_pos2 + f'\\{formatted_date}\\s6x8'
+    path_pos3 = folder_path_pos3 + f'\\{formatted_date}\\s6x8'
+
+    photos_printed_pos1 = get_count_files(path_pos1)
+    photos_printed_pos2 = get_count_files(path_pos2)
+    photos_printed_pos3 = get_count_files(path_pos3)
 
     if (photos_printed_pos1 == -1 | photos_printed_pos2 == -1 | photos_printed_pos3 == -1):
         return jsonify({'error': 'Folder path not found!'})
